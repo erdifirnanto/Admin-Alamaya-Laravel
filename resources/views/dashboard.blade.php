3f26a5ce50484ee138a1a9a10dea29e981f257e6
erdifirnanto <erdifirnanto@gmail.com>
2024-10-21 16:12:09 +0700

    gantidashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Circle Animation</title>
+  <title>Admin Dashboard Alamaya</title>
 </head>
 
 <body>
   <div class="content">
-    <h1 id="title">Laravel Admin Alamaya</h1>
+    <h1 id="title">Laravel Admin Alamayae</h1>
   </div>
   <div class="circle-overlay"></div>
 
