926d4eace7d43a3253cb5ae95a711a8f99fe4c0d
erdifirnanto <erdifirnanto@gmail.com>
2024-10-24 14:12:58 +0700

    fixhomepage

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/homepage', function () {
-        return view('homepage');
-    })->name('homepage');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
