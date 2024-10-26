7f617c2137f8ad541ec4279f6f8fb8b76d4f63be
erdifirnanto <erdifirnanto@gmail.com>
2024-10-26 12:41:27 +0700

    fixeyelogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,23 +26,30 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="email" class="form-label"><strong>Email</strong></label>
-                                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="Enter email" value="{{ old('email') }}" required autofocus>
+                                <input type="email" name="email"
+                                    class="form-control @error('email') is-invalid @enderror" id="email"
+                                    placeholder="Enter email" value="{{ old('email') }}" required autofocus>
                                 @error('email')
-                                <div id="emailHelp" class="form-text text-danger">{{ $message }}</div>
+                                    <div id="emailHelp" class="form-text text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
 
                             <div class="mb-3 position-relative">
-    <label for="password" class="form-label"><strong>Password</strong></label>
-    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" required>
-    <i class="position-absolute" id="togglePassword" style="top: 60%; right: 10px; cursor: pointer;"></i>
-    @error('password')
-    <div id="passwordHelp" class="form-text text-danger">{{ $message }}</div>
-    @enderror
-</div>
+                                <label for="password" class="form-label"><strong>Password</strong></label>
+                                <input type="password" name="password"
+                                    class="form-control @error('password') is-invalid @enderror" id="password"
+                                    required>
+                                <!-- Set initial class for icon -->
+                                <i class="fas fa-eye-slash position-absolute" id="togglePassword"
+                                    style="top: 60%; right: 10px; cursor: pointer;"></i>
+                                @error('password')
+                                    <div id="passwordHelp" class="form-text text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
 
                             <div class="mb-3 d-flex justify-content-end">
-                                <a href="{{ route('password.request') }}" class="text-decoration-none"><strong>Forgot Password?</strong></a>
+                                <a href="{{ route('password.request') }}" class="text-decoration-none"><strong>Forgot
+                                        Password?</strong></a>
                             </div>
 
                             <button type="submit" class="btn btn-dark col-12" style="height: 50px;">Login</button>
@@ -51,30 +58,31 @@
                 </div>
 
                 <div class="col-sm-7 px-0 d-none d-sm-block">
-                    <img src="{{ asset('images/login_image1.png') }}" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+                    <img src="{{ asset('images/login_image1.png') }}" alt="Login image" class="w-100 vh-100"
+                        style="object-fit: cover; object-position: left;">
                 </div>
             </div>
         </div>
     </section>
 
-    <script src="https://kit.fontawesome.com/19ad68a1da.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
-    $(document).ready(function() {
-        $('#togglePassword').click(function() {
-            const passwordField = $('#password');
-            const toggleIcon = $(this);
+        $(document).ready(function() {
+            $('#togglePassword').click(function() {
+                const passwordField = $('#password');
+                const toggleIcon = $(this);
 
-            if (passwordField.attr('type') === 'password') {
-                passwordField.attr('type', 'text');
-                toggleIcon.removeClass('fa-eye-slash').addClass('fa-eye');
-            } else {
-                passwordField.attr('type', 'password');
-                toggleIcon.removeClass('fa-eye').addClass('fa-eye-slash');
-            }
+                // Toggle password visibility and icon class
+                if (passwordField.attr('type') === 'password') {
+                    passwordField.attr('type', 'text');
+                    toggleIcon.removeClass('fa-eye-slash').addClass('fa-eye');
+                } else {
+                    passwordField.attr('type', 'password');
+                    toggleIcon.removeClass('fa-eye').addClass('fa-eye-slash');
+                }
+            });
         });
-    });
-</script>
+    </script>
 </body>
 
 </html>
